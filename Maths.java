2557c08a6fe9c455b80de5887bf52c6b083187b8
Pathan_Amir <155294699+pathan-amir001@users.noreply.github.com>
2024-02-22 13:58:23 +0530

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,13 @@
+package Marvellous;
+
+public class Math
+{
+    public int Addition(int A, int B)
+    {
+        return A+B;
+    }
+}
+
+// javac -d . Maths.java
+// -d : Directory (Folder)
+// . : Current Dictory
